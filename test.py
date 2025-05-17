dcfce50de307fbf3831bb605c2a78b07e2bcf4e6
ysif9 <89231113+ysif9@users.noreply.github.com>
2025-05-17 03:52:55 +0300

    Revert "Added modifications to the DOTGenerator"
    This reverts commit c2ad4cacda5d72411991ead6891b0f8c489f35ef.

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-my_string = "hello world"
-s1 = my_string[0]
-s2 = my_string[1:5]
-s3 = my_string[:5]
-s4 = my_string[6:]
-s5 = my_string[::2]
-s6 = my_string[1:7:2]
-
-numbers_list = [10, 20, 30, 40, 50]
-n_slice = numbers_list[1:-1]
-
-val = True; other = False; n = None
-num_lit = 123; float_lit = 45.67
+if a==5: print("hello")
